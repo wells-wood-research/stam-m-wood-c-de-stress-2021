a99ace3295e2137adc37da05eca5ca66451d5100
MichaelJamesStam <michaelstam@protonmail.com>
2021-04-26 15:26:54 +0100

    Modifying script to randomly sample the structures first.

--- sampling_decoy_pdbs.py
+++ sampling_decoy_pdbs.py
@@ -7,6 +7,7 @@ import os
 import numpy as np
 import pandas as pd
 import subprocess
+import random
 from random import sample
 
 # 1. Setting the file paths and parameters-------------------------------------------
@@ -16,26 +17,36 @@ DECOY_DATA_FOLDER_PATH = os.getenv("DECOY_DATA_FOLDER_PATH")
 DECOY_SUBSET_FOLDER_PATH = os.getenv("DECOY_SUBSET_FOLDER_PATH")
 
 # Setting the seed
-np.random.seed(42)
+random.seed(42)
+
+# Setting the number of structures
+num_structures = 10
 
 # Setting the number of decoys
 num_decoys = 20
 
 # 2. Creating the subset data-------------------------------------------------------------------
 
-# Listing all the sub dirs in the 3DRobot Decoy Data
+# Listing all the sub dirs (structures) in the 3DRobot Decoy Data
 subdirs = [x[0] for x in os.walk(DECOY_DATA_FOLDER_PATH)]
 
 # Removing the DECOY_DATA_FOLDER_PATH from this list
 subdirs[:] = [d for d in subdirs if d not in [DECOY_DATA_FOLDER_PATH]]
 
+# Sampling a subset of these structures
+subdirs_subset = sample(subdirs, num_structures)
+
 # Printing how many structures the script will run for
 print(
-    "Sampling " + str(num_decoys) + " decoys for " + str(len(subdirs)) + " structures"
+    "Sampling "
+    + str(num_decoys)
+    + " decoys for "
+    + str(len(subdirs_subset))
+    + " structures"
 )
 
 # Looping through all the sub directories
-for subdir in subdirs:
+for subdir in subdirs_subset:
 
     # Extracting the name of the subdir
     subdir_name = os.path.basename(subdir)
